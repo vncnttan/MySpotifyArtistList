8936729be823d81c9c6ed8672833f07b23bfa757
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-04-21 23:27:45 +0700

    test

--- src/components/navbar/Navbar-btn.tsx
+++ src/components/navbar/Navbar-btn.tsx
@@ -1,17 +1,20 @@
 import { IconDefinition, IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import style from "./navbar.module.css"
+import Link from "next/link";
+import { Url } from "next/dist/shared/lib/router/router";
 
 interface Props{
     icon ?: IconDefinition;
     tag ?: String;
+    link ?: string;
 }
 
 export default function NavbarBtn({...props}:Props){
     return (
-    <div className={style.navbarbtn + "  text-center"}>
+    <Link className={style.navbarbtn + "  text-center"} href={{ pathname: props.link }}>
         <FontAwesomeIcon icon={props.icon as IconProp} /> <br/>
         <span className="text-center">{props.tag}</span>
-    </div>
+    </Link>
     )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 import style from "./navbar.module.css"
 import NavbarBtn from "./Navbar-btn"
 import { faHouse, faHeart, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-
+import Link from "next/link"
 
 export default function Navbar(){
     return (
     <div id={style.navbar}>
-        <NavbarBtn icon={faHouse} tag="List"/>
-        <NavbarBtn icon={faHeart} tag="Favorites"/>
-        <NavbarBtn icon={faMagnifyingGlass} tag="Search"/>
+        <NavbarBtn icon={faHouse} tag="List" link={"/"}/>
+        <NavbarBtn icon={faHeart} tag="Favorites" link={"/favorites"}/>
+        <NavbarBtn icon={faMagnifyingGlass} tag="Search" link={"/search"}/>
     </div>
     )
 }
