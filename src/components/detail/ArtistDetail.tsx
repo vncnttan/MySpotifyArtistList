d5ffb2ad42d457cce8c5f4f9fce2c625a36b0758
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-04-24 17:48:20 +0700

    Prettify the heading

--- src/components/detail/ArtistDetail.tsx
+++ src/components/detail/ArtistDetail.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import style from "./detail.module.css"
 
 interface Props {
     data ?: any
@@ -8,15 +9,25 @@ export default function ArtistDetail({...props} : Props){
     return (
         <div style={{margin: "1rem"}}>
             <ArtistDetailHeader data={props.data}/>
+            <ArtistDiscography data={props.data} />
         </div>
     )
 }
 
 export function ArtistDetailHeader({...props} : Props){
     return (
-        <div style={{margin: "2rem 0"}}>
-            <Image src={props.data.artist.image} alt={props.data.artist.name + "'s image."} width={200} height={200} style={{display: "inline", borderRadius: "10rem"}}/>
-            <h1 style={{display: "inline", verticalAlign: "bottom", margin: "1rem", fontSize: "3rem", fontWeight: "800"}}> {props.data.artist.name}</h1>
+        <div className={style.container}>
+            <Image src={props.data.artist.image} alt={props.data.artist.name + "'s image."} width={150} height={150} className={style.artistImage}/>
+            <h1 className={style.artistNameHeader}> {props.data.artist.name}</h1>
+        </div>
+    )
+}
+
+export function ArtistDiscography({...props} : Props){
+    return (
+        <div className={style.container}>
+            <h1>Discography</h1>
+            <hr />
         </div>
     )
 }
