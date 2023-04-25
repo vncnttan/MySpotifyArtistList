f1209196fc3677eb3aaba2e01f7ce656789c2a2a
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-04-25 22:41:27 +0700

    t

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@component/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { useState } from 'react';
 
 const client = new ApolloClient({
   uri: 'http://127.0.0.1:8080/query',
@@ -8,9 +9,20 @@ const client = new ApolloClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  let [FavList, setFavList] = useState(()=>{
+    let localStorageJson = localStorage.getItem("favlist");
+    let favlist = []
+    if(localStorageJson){
+      favlist = JSON.parse(localStorageJson)
+    }
+
+    return favlist
+  });
+
   return (
   <ApolloProvider client={client}>
-    <Component {...pageProps} />
+    <Component {...pageProps}/>
   </ApolloProvider>
   )
 }
