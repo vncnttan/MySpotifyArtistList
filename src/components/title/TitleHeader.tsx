09a055aae963ae664dafd5565ada65704d5bb5ca
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-04-27 08:29:45 +0700

    Added the flag on the change language feature

--- src/components/title/TitleHeader.tsx
+++ src/components/title/TitleHeader.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import style from "./title.module.css"
 import { LanguageContext } from "@component/lib/context/LanguageContext";
+import Image from "next/image";
 
 interface Props{
     title ?: string
@@ -12,7 +13,7 @@ export default function TitleHeader({...props}:Props){
     return (
         <div>
             <h1 className={style.title}>{props.title}</h1>
-            <button className={style.langbtn} type="submit" onClick={lang.changeLang}>{lang.userLang}</button>
+            <button className={style.langbtn} type="submit" onClick={lang.changeLang}> <Image alt="flg" src={lang.dict.Flag} width={15} height={15} style={{display: "inline", verticalAlign: "baseline"}}></Image> {lang.userLang}</button>
         </div>
     )
 }
