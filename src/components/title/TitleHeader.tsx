9e8e83f6b8d5e4015e613cba592918c00c86ba0c
vncnttan <vincent.tanjaya.vt@gmail.com>
2023-04-27 23:17:43 +0700

    Revert "Add logo"
    This reverts commit 494f9c6cfb54e6080eec490861873bb65eae1a0f.

--- src/components/title/TitleHeader.tsx
+++ src/components/title/TitleHeader.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import style from "./title.module.css"
 import { LanguageContext } from "@component/lib/context/LanguageContext";
 import Image from "next/image";
-import { useRouter } from "next/router";
 
 interface Props{
     title ?: string
@@ -10,13 +9,10 @@ interface Props{
 
 export default function TitleHeader({...props}:Props){
     let lang = useContext(LanguageContext);
-    let router = useRouter();
-
-    let allLogoRoute = ["/", "/favorites", "/search"]
 
     return (
         <div>
-            <h1 className={style.title}><Image src="./logo.svg" alt="logo" width={20} height={20} style={{display: allLogoRoute.includes(router.pathname) ? "inline" : "none", margin: "0 0.5rem", verticalAlign: "baseline"}} ></Image>{props.title}</h1>
+            <h1 className={style.title}>{props.title}</h1>
             <button className={style.langbtn} type="submit" onClick={lang.changeLang}> <Image alt="flg" src={lang.dict.Flag} width={15} height={15} style={{display: "inline", verticalAlign: "baseline"}}></Image> {lang.userLang}</button>
         </div>
     )
